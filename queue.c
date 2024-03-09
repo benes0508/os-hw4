b9f2d6beab6d69cd7915cf376973ff5c97b0d8db
ben <bene@mail.tau.ac.il>
2024-03-09 11:58:16 +0200

    sdfsdf

--- queue.c
+++ queue.c
@@ -9,14 +9,10 @@ typedef struct Node {
 } Node;
 
 typedef struct {
-    Node* head;
-    Node* tail;
-} ItemQueue;
-
-typedef struct {
     mtx_t lock;
     cnd_t has_items;
-    ItemQueue items;
+    Node* head;
+    Node* tail;
     atomic_size_t waiting;
     atomic_size_t visited;
 } ConcurrentQueue;
@@ -24,8 +20,8 @@ typedef struct {
 ConcurrentQueue queue;
 
 void initQueue(void) {
-    queue.items.head = NULL;
-    queue.items.tail = NULL;
+    queue.head = NULL;
+    queue.tail = NULL;
     mtx_init(&queue.lock, mtx_plain);
     cnd_init(&queue.has_items);
     atomic_store(&queue.waiting, 0);
@@ -35,7 +31,7 @@ void initQueue(void) {
 void destroyQueue(void) {
     mtx_lock(&queue.lock);
 
-    Node* node = queue.items.head;
+    Node* node = queue.head;
     while (node) {
         Node* tmp = node;
         node = node->next;
@@ -54,35 +50,32 @@ void enqueue(void* item) {
     newNode->data = item;
     newNode->next = NULL;
 
-    if (queue.items.tail) {
-        queue.items.tail->next = newNode;
+    if (queue.tail) {
+        queue.tail->next = newNode;
     } else {
-        queue.items.head = newNode;
+        queue.head = newNode;
     }
-    queue.items.tail = newNode;
+    queue.tail = newNode;
 
-    if (atomic_load(&queue.waiting) > 0) {
-        cnd_signal(&queue.has_items);
-    }
-
+    cnd_signal(&queue.has_items);
     mtx_unlock(&queue.lock);
 }
 
 void* dequeue(void) {
     mtx_lock(&queue.lock);
 
-    while (!queue.items.head) {
+    while (!queue.head) {
         atomic_fetch_add(&queue.waiting, 1);
         cnd_wait(&queue.has_items, &queue.lock);
         atomic_fetch_sub(&queue.waiting, 1);
     }
 
-    Node* node = queue.items.head;
+    Node* node = queue.head;
     void* item = node->data;
-    queue.items.head = node->next;
+    queue.head = node->next;
 
-    if (!queue.items.head) {
-        queue.items.tail = NULL;
+    if (!queue.head) {
+        queue.tail = NULL;
     }
 
     free(node);
@@ -97,17 +90,17 @@ bool tryDequeue(void** item) {
         return false;
     }
 
-    if (!queue.items.head) {
+    if (!queue.head) {
         mtx_unlock(&queue.lock);
         return false;
     }
 
-    Node* node = queue.items.head;
+    Node* node = queue.head;
     *item = node->data;
-    queue.items.head = node->next;
+    queue.head = node->next;
 
-    if (!queue.items.head) {
-        queue.items.tail = NULL;
+    if (!queue.head) {
+        queue.tail = NULL;
     }
 
     free(node);
@@ -120,7 +113,7 @@ bool tryDequeue(void** item) {
 size_t size(void) {
     size_t count = 0;
     mtx_lock(&queue.lock);
-    Node* current = queue.items.head;
+    Node* current = queue.head;
     while (current) {
         ++count;
         current = current->next;
